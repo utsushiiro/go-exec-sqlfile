0d3c53a0a1589896c4f041dc2a43b2b9383ca297
utsushiiro <ozeki.haru@gmail.com>
2023-09-16 05:02:52 +0900

    add commented out sqls

--- db/initdb.d/02_seed.sql
+++ db/initdb.d/02_seed.sql
@@ -2,6 +2,10 @@ insert into users (id) values (1);
 insert into users (id) values (2);
 
 insert into posts (id, author_id, content) values (1, 1, 'Hello, world!');
+-- insert into posts (id, author_id, content) values (2, 1, 'Comment Out');
 insert into posts (id, author_id, content) values (2, 1, '(;_;)');
 insert into posts (id, author_id, content) values (3, 2, 'Hello, world!');
+/*
+ * insert into posts (id, author_id, content) values (3, 2, 'Comment Out');
+ */
 insert into posts (id, author_id, content) values (4, 2, 'insert into posts (id, author_id, content) values (4, 2, ''Hello, world!'');');
